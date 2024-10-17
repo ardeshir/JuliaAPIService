248744a169fd90b6be92a0d1a739f5d5142b308a
ardeshir <ardeshir.org@gmail.com>
2024-10-17 10:01:40 -0500

    fix

--- web/genie.jl
+++ web/genie.jl
@@ -1,6 +1,6 @@
 using Genie, Genie.Requests, Genie.Renderer.Json 
 
-rout("/", method = POST) do
+route("/", method = POST) do
     @show jsonpayload()
     @show rawpayload()
     json("$(jsonpayload())")
